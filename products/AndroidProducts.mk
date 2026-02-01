d4b6b83cf63972236e98a3259eba1e164af790dd
kylieeXD <khayloaftck@gmail.com>
2026-02-01 10:37:24 +0000

    vendor: Add POCO X3 NFC (surya)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -15,11 +15,13 @@
 # Product Makefiles
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/beryllium/rvos_beryllium.mk \
-    $(LOCAL_DIR)/ginkgo/rvos_ginkgo.mk\
-    $(LOCAL_DIR)/marble/rvos_marble.mk
+    $(LOCAL_DIR)/ginkgo/rvos_ginkgo.mk \
+    $(LOCAL_DIR)/marble/rvos_marble.mk \
+    $(LOCAL_DIR)/surya/rvos_surya.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     rvos_beryllium-userdebug \
     rvos_ginkgo-userdebug \
-    rvos_marble-userdebug
+    rvos_marble-userdebug \
+    rvos_surya-userdebug
--- target/product/maintainer.mk
+++ target/product/maintainer.mk
@@ -1,3 +1,4 @@
 Rve
 Hirokixd
 Pillows
+kylieeXD
